15c1ab54d0228b0a8a226da5143c199c0edeabbc
wiseman2star <wiseman2star@gmail.com>
2021-10-06 20:47:12 -0600

    created methods for dealer

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -8,5 +8,18 @@ class director:
         self.points = 300
         self.dealer = dealer()
 
+    def game_start(self):
+        while self.game_on:
+            self.get_inputs()
+            self.get_updates()
+            self.do_outputs()
+    
+    def get_inputs(self):
+        pass
 
+    def get_updates(self):
+        pass
+
+    def do_outputs(self):
+        pass
 
