a3bec258c71a1311c6f58d5ec46fe8b96ac53d28
corniab <bencornia@byui.edu>
2021-10-09 17:08:57 -0600

    fixed package structure so that imports run correctly

--- hilo/__main__.py
+++
@@ -1,9 +0,0 @@
-# TODO: Add entry point code here 
-#Daniel Emerson
-#import everything from the director class
-#creating a variable that relates to the director class
-#initializing the game from the game_start method
-
-from game.director import director
-director = director()
-director.game_start()
--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,8 +1,8 @@
-from game.director import director
 import random
 
-#The class constructor
-        #Define deal cards, get points, can deal, init 
+# The class constructor
+# Define deal cards, get points, can deal, init
+
 
 class dealer:
     pass
@@ -10,34 +10,31 @@ class dealer:
     def __init__(self):
         self.current_cards = []
         self.score = 0
-        
-    
+
     def deal_cards(self):
-        card1 = random.randint(1,13)
-        card2 = random.randint(1,13)
+        card1 = random.randint(1, 13)
+        card2 = random.randint(1, 13)
         self.current_cards = [card1, card2]
 
-
     def get_points(self):
-        ''' Calulate the points earned for an individual guess. 
+        """Calulate the points earned for an individual guess.
         Correct = +100
         Incorrect = -100
         Author: Mitchell Ball
-        '''
+        """
         if self.current_cards[0] > self.current_cards[1]:
-            self.score = self.score -75
+            self.score = self.score - 75
 
         elif self.current_cards[0] < self.current_cards[1]:
             self.score = self.score + 100
 
         return self.score
 
-
-        
-    
     def can_deal(self):
         # Checks to see if the player has lost the game
         # and if they whish to continue
         # Is to be used in an IF statement
-        return (self.director.points != 0 )
-    #Morgan Allen
+        # return (self.director.points != 0 )
+        # Morgan Allen
+
+        pass
